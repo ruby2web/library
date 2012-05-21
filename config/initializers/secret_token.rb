932ca66bfd866882d8ff307c4b7d5ae27e5113a9
henoke <henokeh@metropolia.fi>
2012-05-21 01:07:31 -0700

    initial rail app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Library::Application.config.secret_token = '318cd9fb44c21f4bfd02c2de493d8c662bb17b021e021decf66a2c764ebaf0a0f6fc3eaae43f4ec8507085b4521bbb4feb0028e2a0f8e7e33c4feb4ed2998c31'
